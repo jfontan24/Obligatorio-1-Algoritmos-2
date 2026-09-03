7b470321cd0f71ae644769cfb66f7498b47ff693
vpouu <victoriapou0@gmail.com>
2026-09-03 15:39:00 -0300

    agrego archivos moneda, pintura y funciones de ambos

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -7,5 +7,7 @@ using namespace std;
 
 int main()
 {
+    bst<int>* arbolMoneda = new avl<int>();
+    bst<string>* arbolPintura = new avl<string>();
     return 0;
 }
---
+++ funcionesPintura.cpp
@@ -0,0 +1,3 @@
+#pragma once
+#include "avl.cpp"
+#include <string>
